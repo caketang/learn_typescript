5766905e93aff4f60e0a2f283f3b39cd4e17a64a
Gxf <gxf920206@gmail.com>
2019-10-15 18:46:16 +0800

    let const array turple function

--- let.js
+++ let.js
@@ -1,2 +1,14 @@
 var a;
-a = function () { };
+var b;
+//数组生命2种方法
+var c = [1, 2, 3];
+var d = [111, 22];
+// turple 跟数组差不多 里面内容固定 顺序不能变
+var h = ['2,', 2];
+h.push(1);
+console.log(h);
+// function 
+var j = function (a, b) {
+    console.log(a + b);
+};
+j('1', '22');
--- let.ts
+++ let.ts
@@ -1,2 +1,14 @@
-let a:number 
-a = 1
+var a:number;
+let b:number;
+//数组生命2种方法
+let c: Array<number> = [1,2,3]
+let d: number[] = [111,22]
+// turple 跟数组差不多 里面内容固定 顺序不能变
+let h: [string, number] = ['2,', 2]
+h.push(1)
+console.log(h)
+// function 
+let j = function(a: string, b:string):void {
+    console.log(a+b)
+}
+j('1','22')
